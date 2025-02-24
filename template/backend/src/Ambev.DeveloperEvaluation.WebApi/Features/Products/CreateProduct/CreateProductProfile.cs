6b7fd4edf86d602898001a271a8be5b83a717e85
Lucas Brilhante <lbrilhante82@gmail.com>
2025-02-24 12:02:37 -0300

    Feat: Add create product controller and its dependecies

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Features/Products/CreateProduct/CreateProductProfile.cs
@@ -0,0 +1,22 @@
+﻿using Ambev.DeveloperEvaluation.Application.Products.CreateProduct;
+using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.CreateUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Products.CreateProduct;
+
+/// <summary>
+/// Profile for mapping between Application and API CreateProduct responses
+/// </summary>
+public class CreateProductProfile : Profile
+{
+    /// <summary>
+    /// Initializes the mappings for CreateProduct feature
+    /// </summary>
+    public CreateProductProfile()
+    {
+        CreateMap<CreateProductRequest, CreateProductCommand>();
+        CreateMap<CreateProductResult, CreateProductResponse>();
+    }
+}
+
