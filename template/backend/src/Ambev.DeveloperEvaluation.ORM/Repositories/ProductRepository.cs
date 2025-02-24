5f45458a3faa45f6c7082135e52159928cf1f631
Lucas Brilhante <lbrilhante82@gmail.com>
2025-02-23 21:17:32 -0300

    Feat: Create Repository and database configuration for product

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Repositories/ProductRepository.cs
@@ -0,0 +1,78 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using Ambev.DeveloperEvaluation.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ambev.DeveloperEvaluation.ORM.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly DefaultContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of ProductRepository
+        /// </summary>
+        /// <param name="context">The database context</param>
+        public ProductRepository(DefaultContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Creates a new product in the repository
+        /// </summary>
+        /// <param name="product">The product to create</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The created product</returns>
+        public Task<Product> CreateAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retrieves products in the repository
+        /// </summary>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The product if found, null otherwise</returns>
+        public Task<IList<Product>?> GetAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Retrieves a product by their unique identifier
+        /// </summary>
+        /// <param name="id">The unique identifier of the product</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The product if found, null otherwise</returns>
+        public Task<Product?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Update a product in the repository
+        /// </summary>
+        /// <param name="product">The product to update</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>The updated product</returns>
+        public Task<Product> UpdateAsync(Product product, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Deletes a product from the repository
+        /// </summary>
+        /// <param name="id">The unique identifier of the product to delete</param>
+        /// <param name="cancellationToken">Cancellation token</param>
+        /// <returns>True if the product was deleted, false if not found</returns>
+        public Task<bool> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
